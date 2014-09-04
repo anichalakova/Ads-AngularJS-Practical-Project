d606ac7a81d6426e240e7d3cdf98195d6a633855
anichalakova <anichalakova@users.noreply.github.com>
2014-09-04 23:57:35 +0300

    Added JS_4.9.2014

---
+++ JavaScript_04.09.2014/2_TheNumbers.js
@@ -0,0 +1,36 @@
+﻿function dessifferNumbers(input) {
+	var numbers = input[0].split(/\D+/);
+
+	function cleanArray(array, deleteValue) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i] == deleteValue) {         
+				array.splice(i, 1);
+				i--;
+			}
+		}
+		return array;
+	}
+
+	function convertDecToHex(numberString) {
+		var number = parseInt(numberString);
+		var numberString = number.toString(16);
+		return numberString.toUpperCase();
+	}
+
+	function pad(number) {
+		while (number.length < 4) number = '0' + number;
+		number = '0x' + number;
+		return number;
+	}
+	
+	cleanArray(numbers, '');
+	var result = [];
+	for (var i = 0; i < numbers.length; i++) {		
+		numbers[i] = convertDecToHex(numbers[i]);		
+		numbers[i] = pad(numbers[i]);
+		result.push(numbers[i]);		
+	}
+
+	console.log(result.join('-'));	
+}
+dessifferNumbers('482vMWo(*&^%$213;k!@41341((()&^>><///]42344p;e312');
---
+++ JavaScript_04.09.2014/4_CloudManager.js
@@ -0,0 +1,56 @@
+﻿function manageFiles(input) {
+	var result = {};
+
+	for (var i in input) {
+		var tokens = input[i].split(/[ ]+/);
+		var filename = tokens[0].trim();		
+		var extension = tokens[1].trim();		
+		var size = parseFloat(tokens[2]);
+		
+		
+		var totalSize = parseFloat("0");
+		
+		if (!result[extension]) {
+			result[extension] = {};
+			result[extension] = {files:[], memory: totalSize}
+		}
+
+		result[extension]['files'].push(filename);
+			
+		result[extension]['memory'] += parseFloat(size);
+		
+
+	}
+
+	for (var extension in result) {
+		result[extension]['memory'] = result[extension]['memory'].toFixed(2);		
+	}	
+
+
+	result = sortObjectProperties(result);
+
+	for (var extension in result) {
+		result[extension]['files'].sort(function (a, b) {
+			return a > b ? 1 : -1;
+		});		
+	}
+
+	function sortObjectProperties(obj) {
+		var keysSorted = Object.keys(obj).sort();
+		var sortedObj = {};
+		for (var i = 0; i < keysSorted.length; i++) {
+			var key = keysSorted[i];
+			sortedObj[key] = obj[key];
+		}
+		return sortedObj;
+	}
+	console.log(JSON.stringify(result));
+}
+
+manageFiles(['sentinel .exe 15MB',
+'zoomIt .msi 3MB',
+'skype .exe 45MB',
+'trojanStopper .bat 23MB',
+'kindleInstaller .exe 120MB',
+'setup .msi 33.4MB',
+'winBlock .bat 1MB']);
