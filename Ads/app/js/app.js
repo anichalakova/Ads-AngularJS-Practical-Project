908ef88a27f729ce50c3d26e591e57b95cf0fb1f
anichalakova <anichalakova@users.noreply.github.com>
2015-01-02 02:04:59 +0200

    repaired routing

--- Ads/app/js/app.js
+++ Ads/app/js/app.js
@@ -1,7 +1,5 @@
 var app = angular.module('ads', ['ngRoute'])
         .config(function($routeProvider){
             $routeProvider.when('/register', {
-                templateUrl: '/templates/register.html',
-                controller: 'MainController'
-            });
+                templateUrl: '/templates/register.html', controller: 'MainController' });
         });
