c165a5c6a9cdc309295a724ff363e6ddbeac895f
anichalakova <anichalakova@users.noreply.github.com>
2015-01-04 01:00:14 +0200

    added towns and categories sidebars

--- Ads/app/js/controllers/MainController.js
+++ Ads/app/js/controllers/MainController.js
@@ -2,4 +2,12 @@ app.controller('MainController', function($scope, mainData){
     mainData.getAllAds(function(resp){
         $scope.data = resp;
     });
+    
+    mainData.getAllTowns(function(resp){
+        $scope.towns = resp;
+    });
+    
+    mainData.getAllCategories(function(resp){
+        $scope.categories = resp;
+    });
 });
