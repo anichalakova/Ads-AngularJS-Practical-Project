8275076a345d5e1451f59098fe92581f387c7a76
anichalakova <anichalakova@users.noreply.github.com>
2015-01-03 02:14:44 +0200

    show all ads functionality & routing

--- Ads/app/js/app.js
+++ Ads/app/js/app.js
@@ -2,4 +2,9 @@ var app = angular.module('ads', ['ngRoute'])
         .config(function($routeProvider){
             $routeProvider.when('/register', {
                 templateUrl: '/templates/register.html', controller: 'MainController' });
+            $routeProvider.when('/login', {
+                templateUrl: '/templates/login.html', controller: 'MainController' });
+            $routeProvider.when('/ads', {
+                templateUrl: '/templates/all-ads.html', controller: 'MainController' });
+            $routeProvider.otherwise({redirectTo: '/ads'});
         });
