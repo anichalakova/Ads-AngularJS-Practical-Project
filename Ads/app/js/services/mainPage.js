48091ec5e99c75abc04e8e33f9f2ec1a26dfef11
anichalakova <anichalakova@users.noreply.github.com>
2015-01-04 00:28:22 +0200

    added templates/all-ads.html and templates/login.html

---
+++ Ads/app/js/services/mainPage.js
@@ -0,0 +1,14 @@
+app.factory('mainData', function($http, $log){
+    return{
+        getAllAds: function(success) {
+            $http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads'})
+                    .success(function(data, status, headers, config){
+                        success(data);
+                    })
+                    .error(function(data, status, headers, config){
+                        $log.warn(data);
+                    });
+        }
+    };
+    
+}); 
