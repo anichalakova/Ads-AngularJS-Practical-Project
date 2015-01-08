ca22491c78e6fc5d857457fe738070dc6f3ee567
anichalakova <anichalakova@users.noreply.github.com>
2015-01-08 23:53:23 +0200

    show public ads extracted to new controller

--- Ads/app/js/controllers/HomeController.js
+++ Ads/app/js/controllers/HomeController.js
@@ -1,28 +1,5 @@
-app.controller('HomeController', function($scope, mainData){   
+app.controller('HomeController', function($scope){   
     
-    $scope.pageTitle = 'Home';
-    
-    mainData.getAllAds(function(resp){
-        $scope.data = resp;
-    });
-    
-    mainData.getAllTowns(function(resp){
-        $scope.towns = resp;
-    });
-    
-    mainData.getAllCategories(function(resp){
-        $scope.categories = resp;
-    });
-    
-    $scope.selectedNavId = null;
-    
-    $scope.selectedCatId = null;
-    $scope.setSelectedCat = function (selectedId) {
-       $scope.selectedCatId = selectedId;
-    };
-    
-    $scope.selectedTownId = null;
-        $scope.setSelectedTown = function (selectedId) {
-       $scope.selectedTownId = selectedId;
-    };
+    $scope.pageTitle = 'Home';    
+
 });
